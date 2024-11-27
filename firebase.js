c183c9e4dddd0b4cadfdb955f4b073c7cbbe3fe2
Cevresel <ekokredital90@gmail.com>
2024-11-27 22:21:13 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+ript type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCF9TuF50CGnlyr79ZP4z1ZjGDFRQcmsaw",
+    authDomain: "ekoloj-8589e.firebaseapp.com",
+    projectId: "ekoloj-8589e",
+    storageBucket: "ekoloj-8589e.firebasestorage.app",
+    messagingSenderId: "27508329885",
+    appId: "1:27508329885:web:2f161b62f1a3f8ee8b7374"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
